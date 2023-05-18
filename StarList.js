e92931d03684f5ea21484961aebb5f231f9df413
Blue-cupcake <74243979+Blue-cupcake@users.noreply.github.com>
2023-05-18 13:50:51 +0530

    Add files via upload

---
+++ StarList.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import { Card, Icon } from "react-native-elements";
+import axios from "axios";
+export default class StarList extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  cardItem: {
+    marginBottom: 10
+  }
+});
